67b00069da4daa0b791767ebbc50f23ebf034b8c
EduPC <edugbuz343@gmail.com>
2023-07-23 20:19:38 +0100

    Deleted hideAllElements from App

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,3 @@ import { InitialScreen } from './InitialScreen'
 export const App = () => {
   return (<InitialScreen />)
 }
-
-const hideAllElements = () => {
-  let childNodes = document.getElementById("root")?.childNodes
-
-  childNodes?.forEach(e => (e as Element).classList.add("begone"))
-}
