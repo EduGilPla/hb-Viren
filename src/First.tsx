d332773eb047f2207af4a58646989e6a099952bc
EduPC <edugbuz343@gmail.com>
2023-07-25 14:54:01 +0100

    Updated CSS, encased answer buttons in a div

--- src/First.tsx
+++ src/First.tsx
@@ -1,5 +1,6 @@
 
 import { Lose } from "./Lose"
+import { Second } from "./Second"
 import { transitionTo } from "./transition"
 
 export const First = () => {
@@ -12,15 +13,17 @@ export const First = () => {
                     Si alguien (de manera posiblemente accidental) mata a 50 niños y empieza a gritar que es un extremista religioso, ¿qué haría Viren?
                 </h3>
             </div>
-            <button onClick={() => transitionTo(Lose())}>
-                Le confrontaría físicamente
-            </button>
-            <button onClick={() => transitionTo(Lose())}>
-                Llamaría a la polícia (poco ACAB de su parte)
-            </button>
-            <button onClick={() => transitionTo(First())}>
-                Le diría que no es un extremista religioso, solo ha cometido una extremistareligiosidad
-            </button>
+            <div className="answers">
+                <button onClick={() => transitionTo(Lose())}>
+                    Le confrontaría físicamente
+                </button>
+                <button onClick={() => transitionTo(Lose())}>
+                    Llamaría a la polícia (poco ACAB de su parte)
+                </button>
+                <button onClick={() => transitionTo(Second())}>
+                    Le diría que no es un extremista religioso, solo ha cometido una extremistareligiosidad
+                </button>
+            </div>
         </div>
     )
 }
