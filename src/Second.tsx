ab0db32666c9a00b7e9893d536ccd127625f3eb4
EduPC <edugbuz343@gmail.com>
2023-07-25 14:54:17 +0100

    Added Second page

---
+++ src/Second.tsx
@@ -0,0 +1,31 @@
+
+import { Lose } from "./Lose"
+import { transitionTo } from "./transition"
+
+export const Second = () => {
+
+    return (
+        <div className="incomingElement">
+            <h1>SEGUNDA PREGUNTA</h1>
+            <div className="card">
+                <h3>
+                    ¿Cuál fue el día en el que quedaste con el amor de tu vida (Eduardo Gil Plasencia) por primera vez?
+                </h3>
+            </div>
+            <div className="answers">
+                <button onClick={() => transitionTo(Lose())}>
+                    Who
+                </button>
+                <button onClick={() => transitionTo(Lose())}>
+                    16/09/2023
+                </button>
+                <button onClick={() => transitionTo(Third())}>
+                    16/09/2022
+                </button>
+                <button onClick={() => transitionTo(Lose())}>
+                    10/05/2022
+                </button>
+            </div>
+        </div>
+    )
+}
