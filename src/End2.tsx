ee235bc938ebf1249242621340e6a295f34beb9f
EduPC <edugbuz343@gmail.com>
2023-07-26 01:11:55 +0100

    Made final video considerably larger

--- src/End2.tsx
+++ src/End2.tsx
@@ -3,12 +3,14 @@ export const End2 = () => {
 
     return (
         <div className="incomingElement">
-            <h1>VICTORIA (Parte 2)</h1>
-            <div className="card">
-                <h3>
-                    Reacción honesta de Viren a cumplir 23 (sigue todo puto)
-                </h3>
-                <video src="VirenHonestReaction.mp4" id="video" autoPlay loop height={"300em"} ></video>
+            <div className="finalVideoDiv">
+                <h1>VICTORIA (Parte 2)</h1>
+                <div className="card">
+                    <h3>
+                        Reacción honesta de Viren a cumplir 23 (sigue todo puto)
+                    </h3>
+                    <video src="VirenHonestReaction.mp4" id="video" autoPlay loop height={"300em"} ></video>
+                </div>
             </div>
         </div >
     )
